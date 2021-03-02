1a8aac7863ed7cf25367166f815010957a16cc10
Mohamed Alaa <mohamedalaaabas93@gmail.com>
2021-03-02 17:50:54 +0200

    add layout route for Template

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-var path = require('path');
+// var app = express();
+// var path = require('path');
+// const exphbs = require('handlebars');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // app.set('views', path.join(__dirname, 'views/Molo'));
-  // app.use(express.static(path.join(__dirname, '/views/Molo')));
-  res.render(`${__dirname}/../views/Molo`, { Path: "/Molo" });
-  // res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {  
+  res.render(`${__dirname}/../views/Molo`, { Path: "/Molo", layout: 'Molo/layout'});
 });
 
 module.exports = router;
